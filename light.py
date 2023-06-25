68abede8c633a6335bc0810dd12e24f87d8b97d4
mohsenksr <mohsen.kassiri@live.com>
2023-06-25 22:38:55 +0330

    Add light codes

--- light.py
+++ light.py
@@ -1,5 +1,8 @@
+import RPi.GPIO as GPIO
+from statics import LIGHT_PIN
+
 def turn_light_on():
-    pass
+    GPIO.output(LIGHT_PIN, GPIO.HIGH)
 
 def turn_light_off():
-    pass
+    GPIO.output(LIGHT_PIN, GPIO.LOW)
