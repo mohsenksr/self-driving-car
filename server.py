ed0f9cdbf892e1d48ff5e87ac06e049adb5ea2ef
Danial Erfanian <erfaniiyanerfan@gmail.com>
2023-07-01 12:59:46 +0330

    Add live-demo.html

--- server.py
+++ server.py
@@ -1,8 +1,19 @@
+import random
+
 from sanic import Sanic
 from sanic.response import text
+from sanic_ext import render
 
 app = Sanic("MyHelloWorldApp")
-data = {}
+
+data = {
+    "Light": "on",
+    "Front": 24.3333 + random.random() * 5,
+    "Right": 64.3333 + random.random() * 5,
+    "Left": 214.3333 + random.random() * 5,
+    "LightSensor": random.random() * 60 + 40,
+    "DCMotor": "on"
+}
 
 
 @app.get("/")
@@ -10,8 +21,17 @@ async def get_sensor_data(request):
     print(data)
     result = ""
     for sensor_name, value in data.items():
-        result += "\n" + f"Sensor {sensor_name}: {value}"
-    return text(result)
+        result += "<br>" + f"{sensor_name}: {value}"
+    return await render(
+        "live-demo.html", context={
+            **data,
+            "text": result,
+            "max_sensor": 300,
+            "min_sensor": 1,
+
+        }, status=200
+    )
+    # return text(result)
 
 
 @app.put("/")
